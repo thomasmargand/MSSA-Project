d74b15fdccfb68c76735e41663dd35027c63376e
thomasmargand <thomas.c.margand@outlook.com>
2018-06-11 01:19:08 -0400

    Update

---
+++ Code/Navigation.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Skydiving
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Navigation : Page
+    {
+        public Navigation()
+        {
+            this.InitializeComponent();
+        }
+
+        private void navigate_Menu(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "CategoryA1":
+                    NavMenu.Navigate(typeof(CategoryA1));
+                    break;
+                case "CategoryA2":
+                    NavMenu.Navigate(typeof(CategoryA2));
+                    break;
+            }
+        }
+
+        private void Options_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                NavMenu.Navigate(typeof(CategoryA1));
+            }
+            else
+            {
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                navigate_Menu(item as NavigationViewItem);
+            }
+        }
+    }
+}
